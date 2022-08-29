770aec694664c15b1f76ad643143a4f974473b0f
Agung Firmansyah <agungfir98@gmail.com>
2022-08-29 20:10:22 +0700

    add checkHasVote functionality

--- utils/index.js
+++ utils/index.js
@@ -1,3 +1,9 @@
 export const isAdmin = (eventResult, userId) => {
   return eventResult.toString() === userId;
 };
+
+export const checkHasVote = (data, userId) => {
+  return data.filter((v) => {
+    if (v.voter.id === userId) return v;
+  })[0].hasVoted;
+};
