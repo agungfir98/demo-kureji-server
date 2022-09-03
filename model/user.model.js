8c8470d6a3e4027a9b724c2d147df4c93f8553b3
Agung Firmansyah <agungfir98@gmail.com>
2022-09-03 15:40:43 +0700

    add timeStamp and edit voteParticipation model

--- model/user.model.js
+++ model/user.model.js
@@ -2,33 +2,32 @@ import mongoose from "mongoose";
 
 const User = mongoose.model(
   "User",
-  new mongoose.Schema({
-    email: {
-      type: String,
-      required: true,
-    },
-    name: {
-      type: String,
-      required: true,
-    },
-    password: {
-      type: String,
-      required: true,
-    },
-    organization: [
-      { type: mongoose.SchemaTypes.ObjectId, ref: "Organization" },
-    ],
-    voteParticipation: [
-      {
-        eventId: String,
-        organization: String,
-        voteTitle: String,
+  new mongoose.Schema(
+    {
+      email: {
+        type: String,
+        required: true,
       },
-    ],
-    tokenVersion: {
-      type: Number,
-      default: 0,
+      name: {
+        type: String,
+        required: true,
+      },
+      password: {
+        type: String,
+        required: true,
+      },
+      organization: [
+        { type: mongoose.SchemaTypes.ObjectId, ref: "Organization" },
+      ],
+      voteParticipation: [
+        { type: mongoose.SchemaTypes.ObjectId, ref: "Voteevent" },
+      ],
+      tokenVersion: {
+        type: Number,
+        default: 0,
+      },
     },
-  })
+    { timestamps: true }
+  )
 );
 export default User;
