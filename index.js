514a1d2a595a61dff9aaef6151645f09712ac8dd
agungfir98 <agungfir98@gmail.com>
2023-08-28 11:26:22 +0700

    feat: handle static image

--- index.js
+++ index.js
@@ -6,12 +6,16 @@ import mongoose from "mongoose";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import useRouter from "./router/index.js";
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
 const { PORT, ORIGIN } = process.env;
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ORIGIN,
+    origin: [ORIGIN, "http://localhost:5173"],
     credentials: true,
   })
 );
@@ -22,6 +26,8 @@ mongoose
   .catch((e) => console.log(e));
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", useRouter);
 
